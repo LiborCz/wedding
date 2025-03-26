dc984ac00da3dddd8cb778b59341ade251bcf552
Libor Pilny <libor@seznam.cz>
2025-03-27 00:19:36 +0100

    feat: stable

--- src/pages/wedding.tsx
+++ src/pages/wedding.tsx
@@ -1,10 +1,17 @@
 export default function Wedding() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <img src="/img/ring.ico" className="logo w-10 h-10" alt="Rings..." />
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className="font-Wedding">About</h1>
+    <section className="m-10 h-full flex flex-col items-center relative">
+      <img src="/img/bg-nadvori.png" className="w-4/5" />
+      <div className="absolute left-0 bottom-0 w-full z-10">
+        <img src="/img/us.png" className="w-1/5" alt="Us..." />
       </div>
+        
+      <div className="absolute left-0">
+          
+        <img src="/img/ring.ico" className="logo w-10 h-10" alt="Rings..." />
+        <span className="font-wedding w-full text-4xl sm:text-6xl lg:text-8xl">Libor & Regina</span>
+      </div>
+        
     </section>
   );
 }
