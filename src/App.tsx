b1a3b89837854322ce8177c09ff69de45371eb81
Libor Pilny <libor@seznam.cz>
2025-03-26 22:04:35 +0100

    chore: stripping extra items

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Wedding from "@/pages/wedding";
 
 function App() {
   return (
     <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-      <Route element={<div>Page not found</div>} path="*" />
+      <Route path="/wedding" element={<Wedding />} />
+      <Route path="*" element={<Navigate to="/wedding" replace/>} />
     </Routes>
   );
 }
