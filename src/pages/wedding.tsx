cff5f36c87558755aa5eca321ecc5375d65cade1
Libor Pilny <libor@seznam.cz>
2025-03-26 23:09:42 +0100

    chore: testing assets

--- src/pages/wedding.tsx
+++ src/pages/wedding.tsx
@@ -1,10 +1,9 @@
-import { title } from "@/components/primitives";
-
 export default function Wedding() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <img src="/img/ring.ico" className="logo w-10 h-10" alt="Rings..." />
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>About</h1>
+        <h1 className="font-Wedding">About</h1>
       </div>
     </section>
   );
