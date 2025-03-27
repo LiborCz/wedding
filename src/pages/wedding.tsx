59c1a3c923ada79e9209a6172d40788bfe28fbf5
Libor Pilny <libor@seznam.cz>
2025-03-27 01:38:23 +0100

    feat: Responsiveness

--- src/pages/wedding.tsx
+++ src/pages/wedding.tsx
@@ -1,6 +1,6 @@
 export default function Wedding() {
   return (
-    <div className="w-full text-center">
+    <div className="w-full pb-20text-center no-scrollbar">
       <section className="h-full flex justify-center relative bg-stone-300">
         <img src="/img/bg-nadvori.png" className="w-4/5" />
         <div className="absolute left-0 bottom-0 w-full z-10">
@@ -18,13 +18,13 @@ export default function Wedding() {
         </div>
       </section>
 
-      <section className="mt-20 font-sans text-xl sm:text-2xl lg:text-3xl drop-shadow-xl text-center">
+      <section className="mt-20 p-10 font-sans text-xl sm:text-2xl lg:text-3xl drop-shadow-xl text-center">
         <p className="text-center mb-4">Srdečně Vás zveme na svatbu v Zámku Veltrusy</p>
         <p className="mb-20">...a postupně se zde objeví více informací...</p>
 
         <p className="mb-10">Budeme se moc těšit na setkání</p>
 
-        <p className="">Libor & Regina</p>
+        <p className="mb-20">Libor & Regina</p>
       </section>
 
     </div>
