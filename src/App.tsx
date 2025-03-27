f8c7d25ed28d055dce68b23c8f939a46cf016ddb
Libor Pilny <libor@seznam.cz>
2025-03-27 01:50:44 +0100

    feat: changed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Wedding from "@/pages/wedding";
 
 function App() {
   return (
     <Routes>
-      <Route path="/wedding" element={<Wedding />} />
-      <Route path="*" element={<Navigate to="/wedding" replace/>} />
+      {/* <Route path="/wedding" element={<Wedding />} /> */}
+      {/* <Route path="*" element={<Navigate to="/wedding" replace/>} /> */}
+      <Route path="*" element={<Wedding />} />
     </Routes>
   );
 }
