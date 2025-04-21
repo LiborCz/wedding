fff815106112bc0065af198b4c99ec2ae2190cda
Libor Pilny <libor@seznam.cz>
2025-04-21 15:16:14 +0200

    feat: Maps

--- src/modules/InfoLocations.tsx
+++ src/modules/InfoLocations.tsx
@@ -1,4 +1,4 @@
-
+import {Image} from "@heroui/react";
 
 export default function InfoLocations() {
 
@@ -6,7 +6,7 @@ export default function InfoLocations() {
     <div className="flex flex-col gap-4 mb-4 justify-center-safe">
       <div className="mb-6">
         <p className="mb-2">Zámek Veltrusy - svatebčané mohou až na parkoviště pro svatebčany - 100m od vchodu na nádvoří - na obr. Parking (1)</p>
-        <img src="/img/MapaZamku.png" alt="Mapa s body Zámku a Statku" />
+        <Image isZoomed src="/img/MapaZamku.png" alt="Mapa s body Zámku a Statku" onClick={() => {window.open("/img/MapaZamku.png", "_blank");}} />
       </div>
       <div className="mb-6">
         <p className="mb-2">Přejezd ze zámku na Statek (hostinu) - <a href="https://mapy.com/s/hocetatusa" target="_blank"><u>Link</u></a></p>
