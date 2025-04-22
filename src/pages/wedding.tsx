2358067510325297b4897f5a77f9fb8b0dc6b06d
Libor Pilny <libor@seznam.cz>
2025-04-23 00:29:07 +0200

    chore: English note

--- src/pages/wedding.tsx
+++ src/pages/wedding.tsx
@@ -3,11 +3,13 @@ import {Image} from "@heroui/react";
 
 export default function Wedding() {
   return (
-    <div className="w-full py-1 lg:pt-2 text-center max-w-[90%] mx-auto relative bg-stone-300" style={{maxWidth:"1024px", padding:"1rem"}}>
+    <div className="w-full py-1 lg:pt-1 text-center max-w-[90%] mx-auto relative bg-stone-300" style={{maxWidth:"1024px", padding:"1rem", marginTop:"0"}}>
 
       <div className="flex justify-center mb-4" style={{maxHeight:"80%", marginTop:"1rem", marginBottom:"2rem"}}>
         <Image src="/img/Pozvanka.jpg" alt="Tištěná svatební pozvánka" />
       </div>
+
+      <p className="font-sans text-xs mb-6">FOR ENGLISH CONTENT PLEASE TRANSLATE THE PAGE USING GOOGLE</p>
 
       <InfoAccordion />
 
