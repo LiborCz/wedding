01ea3480ffdb058d0ed51d34842f47fb4ce23344
Libor Pilny <libor@seznam.cz>
2025-04-25 22:48:44 +0200

    feat: People and drive over change

--- src/modules/InfoDriveOver.tsx
+++ src/modules/InfoDriveOver.tsx
@@ -6,7 +6,7 @@ export default function InfoDriveOver() {
     <div className="flex flex-col items-center gap-4 mb-4 justify-center-safe">
 
       <div className="mb-6">
-        <p className="mb-2">Na přejezd ze zámku na Statek 1738 bych doporučoval asi tuto cestu (cca 15min) - <a href="https://mapy.com/s/hocetatusa" target="_blank"><u>Link</u></a><br/>Případně lze i nachat auto na parkovišti "U kempu" (parkoviště pro standardní návštěvníky Zámku) - a lze jít i hezkou procházkou pěšky podél vody. Nutno však vymyslet v tom případě i cestu zpět :-)</p>
+        <p className="mb-2">Na přejezd ze zámku na Statek 1738 bych doporučoval asi tuto cestu (cca 15min) - <a href="https://mapy.com/s/konefaduce" target="_blank"><u>Link</u></a><br/>Případně lze i nachat auto na parkovišti "U kempu" (parkoviště pro standardní návštěvníky Zámku) - a lze jít i hezkou procházkou pěšky podél vody. Nutno však vymyslet v tom případě i cestu zpět :-)</p>
         <Image isZoomed src="/img/MapaPrejezdu.png" alt="Mapa přejezdu ze zámku na Statek" />
       </div>
       <div className="mb-6">
