8128bb4ffd6d61b44798b2aa2bc2afb350771504
Libor Pilny <libor@seznam.cz>
2025-04-27 18:13:13 +0200

    fix: link

--- src/modules/InfoPhotos.tsx
+++ src/modules/InfoPhotos.tsx
@@ -6,7 +6,7 @@ export default function InfoOther() {
   return (
     <div className="flex flex-col gap-4 mb-4 justify-center-safe">
       <div className="my-2">
-        <p style={{overflowWrap: 'break-word'}}><b>Fotky od Vás:</b>{'  '}Poprosím pokud budete mít nějaké hezké fotky z obřadu nebo z odpolední/večerní oslavičky - tak klidně nahrajte udělejte svou složku a pár jich tam nám a pro ostatní nahrajte. Je to Google disk, tak snad by to mělo fungovat a být jednoduchý, kdyby s tím byl problém, dejte vědět ;-): <a href="https://drive.google.com/drive/folders/1DdGKaMy6qoS1YdYkMLrzpGVAf9ah7D5P?usp=sharing" target="_blank"><u>Google Drive Link</u></a>
+        <p style={{overflowWrap: 'break-word'}}><b>Fotky od Vás:</b>{'  '}Poprosím pokud budete mít nějaké hezké fotky z obřadu nebo z odpolední/večerní oslavičky - tak klidně nahrajte udělejte svou složku a pár jich tam nám a pro ostatní nahrajte. Je to Google disk, tak snad by to mělo fungovat a být jednoduchý, kdyby s tím byl problém, dejte vědět ;-): <a href="https://drive.google.com/drive/folders/1DdGKaMy6qoS1YdYkMLrzpGVAf9ah7D5P?usp=sharing" target="_blank"><u>Link</u></a>
         </p>
       </div>
 
