b76e32a34f03993b909af3162faefa30dd1fe580
Libor Pilny <libor@seznam.cz>
2025-06-02 23:32:16 +0200

    feat: photos uploaded

--- src/pages/wedding.tsx
+++ src/pages/wedding.tsx
@@ -23,15 +23,16 @@ export default function Wedding() {
         Děkujeme moc za všechny Vaše dárky, příspěvky, překvapení - ale hlavně za to, že jste k nám na svatbu dorazili, ať už jen na chvilku na samotný obřad (o to víc si toho vážíme) a nebo pak jste s námi strávili čas i při odpoledním pohoštění - bylo nám ctí a radostí se s Vámi potkat a slavit s Vámi. Svou velkou účastí jste nám udělali obrovskou radost.
       </p>
 
-      <p className="font-sans text-[80%] mb-4 p-1" style={{fontSize:"0.8rem", fontWeight:"bold"}}>
-        Ohledně fotek - dnes jsme fotky společně s fotoalbem obdrželi - odkaz najedete níže - ale ještě jsem nestačil všechny projít a vybrat - zkusím hned zítra nebo pozítří - tudiž zatim dám do složky pod odkazem jen pár, takovou ochutnávku :-)... (ale pak přidám i ty, kde jsme společně s Vámi)<br />
-        PS: Jsou tam i ty se všemi hosty na obřadu, ty jsem nechal ve větším rozlišení, aby se daly trochu přiblížit... 
+      <p className="font-sans text-[80%] mb-4 p-1" style={{fontSize:"0.8rem"}}>
+        <b>Ohledně fotek</b> - odkaz najedete níže - vybral jsem cca 100 fotek, které mě přišly nejlepší a zachycují snad dobře hezkou atmosféru... Nebylo z mých silách se dívat, jak tam kdo je (bylo nás tam bylo docela dost...) - tak kdyby se někomu nějaká fotka nelíbila - dejte vědět a já ji schovám :-)<br />
+        Jinak pak mám ještě pár dalších (ať už s námi nebo bez nás), které rozešlu dotyčným osobně a stejně tak i ty rodinné nechávám v jiném albu.<br /><br />
+        PS: Jsou tam i 2 fotky se všemi hosty na obřadu - ty jsem nechal ve větším rozlišení, aby se daly trochu přiblížit...  Kdyby ale někdo chtěl i jinou fotku ve většim rozlišení, tak mě dejte vedět...
       </p>
 
 
       <InfoAccordion />
 
-      <p className="font-sans text-xs mb-6 mt-10">**** poslední aktualizace: 1.června 00:00 ****</p>
+      <p className="font-sans text-xs mb-6 mt-10">**** poslední aktualizace: 2.června 00:00 ****</p>
 
     </div>
   );
